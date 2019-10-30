0339b8f5ce10a5774b704c96c72f0caea3fbdfe6
Kleina13 <ragnarhelgi22@gmail.com>
2019-10-30 09:28:49 +0000

    footer and color

--- app.py
+++ app.py
@@ -21,25 +21,24 @@ for station in data:
     if station['company'] not in stations:
         stations.append(station['company'])
 
+low_bensin = data[0]
+low_disel = data[0]
+for station in data:
+	if station['bensin95'] < low_bensin['bensin95']:
+		low_bensin = station
+	else: pass
+	if station['diesel'] < low_disel['diesel']:
+		low_disel = station
+	else: pass
 
-
-
+	
 @app.route('/')
 def index():
-	low_bensin = data[0]
-	low_disel = data[0]
-	for station in data:
-		if station['bensin95'] < low_bensin['bensin95']:
-			low_bensin = station
-		else: pass
-		if station['diesel'] < low_disel['diesel']:
-			low_disel = station
-		else: pass
 	return rend('index.html', stations=stations, timestamp=timestamp, data=data, low_bensin=low_bensin, low_disel=low_disel)
 
 @app.route('/company/<name>')
 def company(name):
-	return rend('company.html', name=name, data=data)
+	return rend('company.html', name=name, data=data, timestamp=timestamp, low_bensin=low_bensin, low_disel=low_disel)
 
 @app.route('/station/<key>')
 def gas_station(key):
@@ -47,7 +46,7 @@ def gas_station(key):
 		if x['key'] == key:
 			station = x
 			break
-	return rend('station.html', key=key, data=data, station=station)
+	return rend('station.html', key=key, data=data, station=station, timestamp=timestamp, low_bensin=low_bensin, low_disel=low_disel)
 
 
 # error <<<<<<<<<<<
